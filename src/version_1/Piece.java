af75b03f09988bcbe160542457a9e2238349e2d4
Miguel Angel <migguavil@alu.edu.gva.es>
2023-05-29 09:37:07 +0200

    Adding Piece

---
+++ src/version_1/Piece.java
@@ -0,0 +1,13 @@
+package version_1;
+
+public class Piece {
+	private int id;
+	private int position;
+	private boolean isInFinalBox=false;
+	
+	public Piece(int id) {
+		this.id=id;
+		this.position=0;
+	}
+	
+}
