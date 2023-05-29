72d335168ec349c5901f84d9fd508a1f24f9b52a
Miguel Angel <migguavil@alu.edu.gva.es>
2023-05-29 09:51:37 +0200

    Changing Box Constructor

--- src/version_1/Box.java
+++ src/version_1/Box.java
@@ -5,12 +5,12 @@ import version_1.Piece;
 public class Box {
 	private static int boxCounter = 1;
 	private int boxNumber;
-	private Piece firstPiece = null;
-	private Piece secondPiece = null;
-	private boolean isThereBridge = false;
+//	private Piece firstPiece = null;
+//	private Piece secondPiece = null;
+//	private boolean isThereBridge = false;
 	
 	
-	public Box(int boxNumber, Piece firstPiece, Piece secondPiece, boolean isThereBridge) {
+	public Box() {
 		this.boxNumber = boxCounter++;
 	}
 	
