05478b4e968ab1f47c1d606dde38bff97519fe16
Miguel Angel <migguavil@alu.edu.gva.es>
2023-05-29 09:37:14 +0200

    Adding Player

---
+++ src/version_1/Player.java
@@ -0,0 +1,17 @@
+package version_1;
+
+import version_1.Piece;
+
+public class Player {
+	private String color;
+	private String name;
+	private Piece playerPiecespieces[];
+	
+	public Player(String color, String name, Piece[] playerPiecespieces) {
+		this.color = color;
+		this.name = name;
+		this.playerPiecespieces = playerPiecespieces;
+	}
+	
+	
+}
