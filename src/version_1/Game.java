2b37d4ce72ee996d273337dbff701f26def84476
Miguel Angel <migguavil@alu.edu.gva.es>
2023-05-29 09:45:47 +0200

    Simplifying Player

--- src/version_1/Game.java
+++ src/version_1/Game.java
@@ -5,21 +5,17 @@ import version_1.Player;
 
 public class Game {
 	private Player [] players;
-
+	
 
 	public Game(int numberOfPlayers) {
-		Piece [] player1pieces= new Piece [1];
-		Piece [] player2pieces= new Piece [1];
-		this.players[0] = new Player ("red", "Player 1", player1pieces);
-		this.players[1] = new Player ("yellow", "Player 2", player2pieces);
+		this.players[0] = new Player ("red", "Player 1", new Piece [1]);
+		this.players[1] = new Player ("yellow", "Player 2", new Piece [1]);
 		
 		if (numberOfPlayers > 2) {
-			Piece [] player3pieces= new Piece [1];
-			this.players[2] = this.players[2] = new Player ("green", "Player 3", player3pieces);
+			this.players[2] = this.players[2] = new Player ("green", "Player 3", new Piece [1]);
 		}
 		if (numberOfPlayers > 3) {
-			Piece [] player4pieces= new Piece [1];
-			this.players[3] = this.players[3] = new Player ("blue", "Player 4", player4pieces);
+			this.players[3] = this.players[3] = new Player ("blue", "Player 4", new Piece [1]);
 		}
 		
 	}
