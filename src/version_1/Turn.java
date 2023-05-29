81483121cd80438ee82675df2c24e74054bc6fce
Miguel Angel <migguavil@alu.edu.gva.es>
2023-05-29 09:37:41 +0200

    Adding Turn

---
+++ src/version_1/Turn.java
@@ -0,0 +1,14 @@
+package version_1;
+
+import version_1.Player;
+
+public class Turn {
+	private String turnOrder[];
+	private int turnOwner;
+	private Player owner;
+	
+	public Turn(Player[] players) {
+		
+	}
+	
+}
