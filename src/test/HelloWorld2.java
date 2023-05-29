2dbb94b2cc7cba9af3788de576add5a192b59c48
pabcrudel <pabcrudel@gmail.com>
2023-05-29 09:24:41 +0200

    Second commit

---
+++ src/test/HelloWorld2.java
@@ -0,0 +1,10 @@
+package test;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello git 2");
+
+	}
+
+}
---
+++ src/test2/HelloWorld.java
@@ -0,0 +1,10 @@
+package test2;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello there");
+
+	}
+
+}
