49b78ca6d16d03b69ae5c15c0146537eb830aa26
pabcrudel <pabcrudel@gmail.com>
2023-05-30 09:02:45 +0200

    Added default constructor

--- src/version_1/Box.java
+++ src/version_1/Box.java
@@ -6,6 +6,7 @@ public class Box {
 //	private Piece secondPiece = null;
 //	private boolean isThereBridge = false;
 	
+	public Box() {}
 	
 	public Box(int boxNumber) {
 		this.boxNumber = boxNumber;
