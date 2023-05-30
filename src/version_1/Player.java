83a47434eddc4164a934a3c33f338956edf9ef8f
pabcrudel <pabcrudel@gmail.com>
2023-05-30 09:45:27 +0200

    Get random piece from home

--- src/version_1/Player.java
+++ src/version_1/Player.java
@@ -5,11 +5,13 @@ public class Player {
 	private String name;
 	private Piece [] pieces;
 	private HomeBox home;
+	private int startingBox;
 	
-	public Player(String color, String name) {
+	public Player(String color, String name, int startingBox) {
 		this.color = color;
 		this.name = name;
 		this.home = new HomeBox(this.pieces);
+		this.startingBox = startingBox;
 		this.setPieces();
 	}
 
@@ -28,10 +30,26 @@ public class Player {
 	public String getName() {
 		return name;
 	}
+	
+	public int getStartingBox() {
+		return this.startingBox;
+	}
 
 	public Piece [] getPieces() {
 		return pieces;
 	}
 	
-	
+	public Piece getPieceFromHome() {
+		boolean lock = true;
+		Piece piece = null;
+		
+		for (int i = 0; lock && i < pieces.length; i++) {			
+			if (pieces[i].getPosition() instanceof HomeBox) {
+				piece = pieces[i];
+				lock = false;
+			}
+		}
+		
+		return piece;
+	}
 }
