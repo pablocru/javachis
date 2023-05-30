8b756ca3395c944a5d0e83f08b5267e4e5a7641c
pabcrudel <pabcrudel@gmail.com>
2023-05-30 09:04:30 +0200

    simplified data

--- src/version_1/HomeBox.java
+++ src/version_1/HomeBox.java
@@ -1,14 +1,15 @@
 package version_1;
 
 public class HomeBox extends Box{
-	private Player owner;
+	private Piece [] howManyPieces;
 	
-	public Player getOwner() {
-		return owner;
-	}
+	public HomeBox() {}
 
-	public HomeBox(Player owner) {
-		super(0);
-		this.owner = owner;
+	public HomeBox(Piece [] howManyPieces) {
+		this.howManyPieces = howManyPieces;
+	}
+	
+	public boolean anyoneHome() {
+		return howManyPieces != null && howManyPieces.length > 0;
 	}
 }
