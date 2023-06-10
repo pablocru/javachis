68dcd9130e2a53f415d6fba688e5eea3eb298b80
Miguel Ángel Guaita <miguelanguai@gmail.com>
2023-06-10 19:29:37 +0200

    Changes in Database (WIP). Structure of class is completed IMO

--- src/version_1/Database.java
+++ src/version_1/Database.java
@@ -11,8 +11,8 @@ import java.util.ArrayList;
 
 public class Database {
 	private ArrayList<String> usernameArray = new ArrayList<>();
+	private Connection connection;
 
-	
 
 	public Database() {
 	}
@@ -29,69 +29,57 @@ public class Database {
 		//here is the code for database connection
 		Database myDatabase = new Database();
 		//ejemplo de añadido al string
-//		myDatabase.usernameArray[0]="hola";
-//		System.out.println(myDatabase.usernameArray[0]);
-		//starting connection
-		Connection connection=null;
+		//		myDatabase.usernameArray[0]="hola";
+		//		System.out.println(myDatabase.usernameArray[0]);
+
 		//url of the database: javachis is the DB name
 		String url="jdbc:mysql://localhost:33306/javachis"+ ""; 
 		//user of the database: root is selected
 		String user="root";
 		//password is alumnoalumno
 		String password="alumnoalumno";
-		String query="SELECT * from  resultsTable";
-		
-//		//necesito traer dos cosas del juego: el nombre del jugador local y si ha ganado la partida
-//		//este ejemplo hay que cambiarlo al ejemplo del jugador local
-//		Player myPlayer = new Player("example1", 0);
-//		boolean haveIWon=true;
-//		
-//		//primero se realiza un select username de la tabla para que saque todos los username
-//		
-		
-		
-		
+		//starting connection
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-
-			//Trying the connection with the url, user and password given
-			connection=DriverManager.getConnection(url,user,password);
-		}catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}catch (SQLException e) {
+			myDatabase.connection=DriverManager.getConnection(url,user,password); 
+		} catch (Exception e) { 
 			e.printStackTrace();
 		} 
 
-//		showingData(connection, url, user, password);
-		savingUsernamesIntoAnArray(connection, url, user, password, myDatabase);
+		savingUsernamesIntoAnArray(myDatabase);
+		//estas dos variables son ejemplos, despues hay que conectarlas con el Controller
+		String player="porRellenar";
+		boolean winOrLose = true;
+		if (isPlayerInArrayList(myDatabase)) {
+			updatingDatabase (myDatabase,player,winOrLose);
+		}else  insertingPlayerInDatabase (myDatabase,player,winOrLose)
+		
+		showingData(myDatabase);
 	}
-	public static void showingData (Connection connection, String url, String user, String password) {
+
+	public static void showingData (Database exampleDatabase) {
+		//este metodo servirá para mostrar los datos en la interfaz
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection=DriverManager.getConnection(url,user,password); 
-			System.out.println("connected");
-			Statement sentence=connection.createStatement();
+			System.out.println("connected. Showing usernames from ArrayList");
+			Statement sentence=exampleDatabase.connection.createStatement();
 			ResultSet rs=sentence.executeQuery("SELECT username, wonGames FROM resultsTable");
 			while (rs.next()) {
-				System.out.println(rs.getString("username")+", "+rs.getString("wonGames"));
-				//tambien se puede hacer con numeros dependiendo del orden que tenga en la tabla la columna del atributo que se quiere seleccionar
+				System.out.println(rs.getString("username"));
 			}
 		} catch (Exception e) { 
 			e.printStackTrace();
 		}
 	}
-	
-	//method for saving usernames into an array
-	public static void savingUsernamesIntoAnArray (Connection connection, String url, String user, String password, Database exampleDatabase) {
+
+	//method for saving usernames into an arraylist
+	public static void savingUsernamesIntoAnArray (Database exampleDatabase) {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection=DriverManager.getConnection(url,user,password); 
-			System.out.println("connected");
-			Statement sentence=connection.createStatement();
+			System.out.println("connected. Adding usernames to ArrayList");
+			Statement sentence=exampleDatabase.connection.createStatement();
+
 			ResultSet rs=sentence.executeQuery("SELECT username, wonGames FROM resultsTable");
 			while (rs.next()) {
-				System.out.println(rs.getString("username"));
 				String usernameToAdd = rs.getString("username");
 				exampleDatabase.usernameArray.add(usernameToAdd);
 			}
@@ -99,5 +87,21 @@ public class Database {
 			e.printStackTrace();
 		}
 	}
-
+	
+	public static boolean isPlayerInArrayList (Database exampleDatabase) {
+		//este metodo revisa si el jugador está en el arraylist de la base de datos y devuelve un booleano
+		return true;
+	}
+	
+	public static void updatingDatabase (Database exampleDatabase, String player, boolean winOrLose) {
+		//aqui se hace el update del jugador. Se accede SOLO SI EL JUGADOR EXISTE
+		//si ha ganado, se hace +1 a wonGames y a playedGames
+		//si ha perdido, se hace +1 solo a playedGames
+	}
+	
+	public static void insertingPlayerInDatabase (Database exampleDatabase, String player, boolean winOrLose) {
+		//aqui se hace el insert del jugador en la database. Se accede SOLO CUANDO EL JUGADOR NO EXISTE
+		//si ha ganado, se pone 1 en wonGames y a playedGames
+		//si ha perdido, se pone 1 solo en playedGames
+	}
 }
