6ea73c9f26bd7e30fd293ac46e51c2a1a7812251
Miguel Ángel Guaita <miguelanguai@gmail.com>
2023-06-11 14:19:35 +0200

    Changes in database methods (WIP)

--- src/version_1/Database.java
+++ src/version_1/Database.java
@@ -28,9 +28,8 @@ public class Database {
 	public static void main (String [] args) {
 		//here is the code for database connection
 		Database myDatabase = new Database();
-		//ejemplo de añadido al string
-		//		myDatabase.usernameArray[0]="hola";
-		//		System.out.println(myDatabase.usernameArray[0]);
+		Player playerExample = new Player ("Ejemplo",1);
+		
 
 		//url of the database: javachis is the DB name
 		String url="jdbc:mysql://localhost:33306/javachis"+ ""; 
@@ -48,12 +47,11 @@ public class Database {
 		} 
 
 		savingUsernamesIntoAnArray(myDatabase);
-		//estas dos variables son ejemplos, despues hay que conectarlas con el Controller
 		String player="porRellenar";
-		boolean winOrLose = true;
-		if (isPlayerInArrayList(myDatabase)) {
-			updatingDatabase (myDatabase,player,winOrLose);
-		}else  insertingPlayerInDatabase (myDatabase,player,winOrLose);
+		boolean iWon = true; //variable de ejemplo a conectar con el controlador
+		if (isPlayerInArrayList(myDatabase, playerExample)) {
+			updatingDatabase (myDatabase,playerExample,iWon);
+		}else  insertingPlayerInDatabase (myDatabase,player,iWon);
 		
 		showingData(myDatabase);
 	}
@@ -61,7 +59,7 @@ public class Database {
 	public static void showingData (Database exampleDatabase) {
 		//este metodo servirá para mostrar los datos en la interfaz
 		try {
-			System.out.println("connected. Showing usernames from ArrayList");
+			System.out.println("connected. Showing usernames from database");
 			Statement sentence=exampleDatabase.connection.createStatement();
 			ResultSet rs=sentence.executeQuery("SELECT username, wonGames FROM resultsTable");
 			while (rs.next()) {
@@ -88,15 +86,61 @@ public class Database {
 		}
 	}
 	
-	public static boolean isPlayerInArrayList (Database exampleDatabase) {
+	public static boolean isPlayerInArrayList (Database exampleDatabase, Player player1) {
 		//este metodo revisa si el jugador está en el arraylist de la base de datos y devuelve un booleano
-		return true;
+		boolean back=false; //se inicializa a falso
+		for (int i=1;i<exampleDatabase.usernameArray.size();i++) {
+			if (player1.getName()==exampleDatabase.usernameArray.get(i)) {
+				back= true;
+			}else back= false;
+		}
+		return back;
 	}
 	
-	public static void updatingDatabase (Database exampleDatabase, String player, boolean winOrLose) {
+	public static void updatingDatabase (Database exampleDatabase, Player player, boolean iWon) {
 		//aqui se hace el update del jugador. Se accede SOLO SI EL JUGADOR EXISTE
 		//si ha ganado, se hace +1 a wonGames y a playedGames
 		//si ha perdido, se hace +1 solo a playedGames
+		String username=player.getName();
+		int wonGames=0;
+		int playedGames=0;
+		
+		//aqui hago un select de las partidas ganadas y lo guardo en una variable
+		try {
+			Statement sentence=exampleDatabase.connection.createStatement();
+			ResultSet rs=sentence.executeQuery("SELECT wonGames FROM resultsTable WHERE username ='"+username+"'");
+			wonGames = rs.getInt("wonGames");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		//aqui hago un select de las partidas jugadas y lo guardo en otra variable
+		try {
+			Statement sentence=exampleDatabase.connection.createStatement();
+			ResultSet rs=sentence.executeQuery("SELECT playedGames FROM resultsTable WHERE username ='"+username+"'");
+			playedGames = rs.getInt("playedGames");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		//aqui creo dos variables de los valores de partidas ganadas y jugadas actualizada, porque las necesito para el UPDATE
+		int wonGamesUpdated;
+		int playedGamesUpdated;
+		
+		//aqui, si se gana, se iteran ambas y si se pierde, solo la de partidas jugadas
+		if (iWon) {
+			wonGamesUpdated=wonGames+1;
+			playedGamesUpdated=playedGames+1;
+		} else { 
+			playedGamesUpdated=playedGames+1;
+		}
+		try {
+			Statement sentence=exampleDatabase.connection.createStatement();
+			ResultSet rs=sentence.executeQuery("UPDATE resultsTable SET playedGames = "+playedGamesUpdated+"WHERE username ='"+username+"'");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 	
 	public static void insertingPlayerInDatabase (Database exampleDatabase, String player, boolean winOrLose) {
