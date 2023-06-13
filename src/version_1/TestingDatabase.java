4db5c45fe48fa4b97324e004f1823260392ec41e
Miguel Angel <migguavil@alu.edu.gva.es>
2023-06-13 13:10:47 +0200

    creating rollCheatingDice

--- src/version_1/TestingDatabase.java
+++ src/version_1/TestingDatabase.java
@@ -1,20 +1,41 @@
 package version_1;
 
+import java.util.Random;
+
 public class TestingDatabase {
 
 	public static void main(String[] args) {
 
-		Database testDatabase = new Database();
-		System.out.println(Database.howManyPlayersInDatabase());
-		String [][] array = new String [2][3];
-		array = Database.savingDatabasePlayerInto2DArray();
+//		Database testDatabase = new Database();
+//		System.out.println(Database.howManyPlayersInDatabase());
+//		String [][] array = new String [2][3];
+//		array = Database.savingDatabasePlayerInto2DArray();
+//		
+//		for (int i =0; i<array.length; i++) {
+//			for (int j=0; j<array[i].length;j++) {
+//				System.out.println(array[i][j]);
+//			}
+//		}
 		
-		for (int i =0; i<array.length; i++) {
-			for (int j=0; j<array[i].length;j++) {
-				System.out.println(array[i][j]);
-			}
-		}
+		System.out.println(rollDice());
 		
+
+		
+		
+	}
+	
+	public static int rollDice() {
+		Random r = new Random ();
+		int dice;
+		int a = r.nextInt(1,4);
+		int b = r.nextInt(1,5);
+		int c = r.nextInt(5,7);
+		
+		if (a==1) {
+			dice = b;
+		}else dice = c;
+				
+		return dice;
 	}
 
 }
